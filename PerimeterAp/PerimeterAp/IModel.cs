a07e746d4109228086141c02895d89afea874c53
Vladikavkaz <44994888+CommandBlockYT0@users.noreply.github.com>
2021-02-11 20:45:09 +0300

    Added IModel

---
+++ PerimeterAp/PerimeterAp/IModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimeterAp
+{
+    interface IModel
+    {
+        /// <summary>
+        /// Периметр
+        /// </summary>
+        /// <param name="a">А</param>
+        /// <param name="b">Б</param>
+        /// <returns>Периметр</returns>
+        double P(double a, double b);
+    }
+}
